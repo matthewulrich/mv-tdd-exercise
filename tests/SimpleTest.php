a8b64b2bb24e43c120435f0a9629e5bc17634204
Matt Ulrich <phaseblue69@gmail.com>
2018-11-15 18:05:47 -0600

    Finish unit tests for Offering class

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -1,6 +1,9 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use \App\Offering;
+use \App\Investor;
+use \App\Investment;
 
 class SimpleTest extends TestCase
 {
@@ -9,5 +12,43 @@ class SimpleTest extends TestCase
     {
         $foo = true;
         $this->assertTrue($foo);
+    }
+
+    // Test that name is set when Offering is created
+    public function testOfferingIsGivenName()
+    {
+        $originalName = 'Offering 1';
+
+        $offering1 = new Offering('Offering 1', 3);
+
+        $expectedName = $offering1->getName();
+
+        $this->assertEquals($originalName, $expectedName);
+    }
+
+    // Test that maximum investments allowed is set when Offering is created
+    public function testMaximumInvestments()
+    {
+        $originalMaxInvestments = 3;
+
+        $offering1 = new Offering('Offering 1', 3);
+
+        $expectedMaxInvestments = $offering1->getMaxInvestments();
+
+        $this->assertEquals($originalMaxInvestments, $expectedMaxInvestments);
+    }
+
+    // Test that an exceptin is thrown when maximum number of Investments has been reached and their is an attemp to add an Investment to the Offering
+    public function testMaxInvestmentsReached()
+    {
+        $this->expectException(Exception::class);
+
+        $offering1 = new Offering('Offering 1', 3);
+
+        $investor = new Investor('John', 'Smith', 'john@example.com');
+
+        for ($i = 1; $i < 5; $i++) {
+            $investment = new Investment(1000, 'credit card', $offering1, $investor);
+        }
     }
 }
